cc4b068ed44123191cecc3f55dbc5fb45e7066d0
thomson3914 <thomson3914@gmail.com>
2018-08-16 12:19:39 -0400

    fixed port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var path = require("path");
 // Tells node that we are creating an "express" server
 var app = express();
 // Sets an initial port. We"ll use this later in our listener
-var PORT = 8080;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
