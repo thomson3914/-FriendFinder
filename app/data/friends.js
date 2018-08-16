0f4bf88f22519f7aaeefa3c93ea775ae1fa0fc57
thomson3914 <thomson3914@gmail.com>
2018-08-16 10:31:22 -0400

    changed friends array

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,16 +3,16 @@ var friends = [
         name: "Bernie",
         pic: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTI5ODcyMjQ1NDQ2MjA5NTA2/bernie-sanders-official-senate-photojpg.jpg",
         scores: [
+            1,
             5,
-            2,
             3,
             1,
             1,
-            3,
+            1,
+            5,
             5,
             1,
-            3,
-            5
+            3
         ]
     },
 
@@ -20,15 +20,15 @@ var friends = [
         name: "Ryan",
         pic: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE4MDAzNDEwODQ1NDAyNjM4/2012-charlotte-ultraswim-grand-prix.jpg",
         scores: [
-            2,
-            3,
-            4,
             5,
-            2,
-            3,
-            2,
+            1,
             5,
-            4,
+            1,
+            5,
+            1,
+            5,
+            1,
+            5,
             1
         ]
     },
@@ -81,19 +81,19 @@ var friends = [
         ]
     },
     {
-        name: "Gigi",
-        pic: "https://www.biography.com/.image/c_limit%2Ccs_srgb%2Cq_auto:good%2Cw_1400/MTU1MDE2MzQxNzQzNDc3ODc4/gigi-hadid-appears-backstage-at-the-2015-muchmusic-video-awards-at-muchmusic-hq-on-june-21-2015-in-toronto-canada-photo-by-george-pimentel_wireimage.webp",
+        name: "Michelle",
+        pic: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE5NDg0MDU0ODEyNzIyNzAz/michelle-obama-thumb-2.jpg",
         scores: [
-            2,
-            4,
-            1,
             3,
+            4,
+            4,
+            4,
             5,
             2,
             5,
+            3,
             1,
-            2,
-            4
+            5
         ]
     },
     {
@@ -101,14 +101,14 @@ var friends = [
         pic: "https://www.biography.com/.image/c_limit%2Ccs_srgb%2Cq_auto:good%2Cw_1400/MTUzMDQ5MDMyMjQyMDQ2NTg0/cardi-b-arrives-at-the-2017-mtv-video-music-awards-at-the-forum-on-august-27-2017-in-inglewood-california-photo-by-gregg-deguire_getty-images.webp",
         scores: [
             5,
-            2,
-            3,
             4,
-            1,
+            4,
+            3,
+            3,
             4,
             5,
+            4,
             2,
-            3,
             4
         ]
     },
@@ -116,32 +116,32 @@ var friends = [
         name: "Jennifer",
         pic: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTQzMjgyNDgwNjIxODIzNTU5/jennifer-lawrence_gettyimages-626382596jpg.jpg",
         scores: [
-            5,
-            5,
-            5,
-            5,
-            5,
-            5,
-            5,
-            5,
-            5,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
             4
         ]
     },
     {
-        name: "Meghan",
-        pic: "https://www.biography.com/.image/c_limit%2Ccs_srgb%2Cq_auto:good%2Cw_1400/MTU1MjAxMTc3MjcxNjc0Nzc1/meghan-markle-visits-the-iconic-titanic-belfast-during-their-visit-to-northern-ireland-on-march-23-2018-in-belfast-northern-ireland-united-kingdom-photo-by-samir-husseinsamir-husseinwireimage.webp",
+        name: "Sarah",
+        pic: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTU3MjM5ODQ4OTk2OTcyMTQ3/sarah-sanders_photo-by-chip-somodevillagetty-images-square.jpg",
         scores: [
-            4,
             5,
             5,
-            2,
-            1,
-            4,
-            2,
-            3,
             5,
-            2
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
         ]
     }
 ];
