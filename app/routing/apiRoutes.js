cddd847bd0975e8253e976c4d3ddfa47261963e1
thomson3914 <thomson3914@gmail.com>
2018-08-16 11:19:22 -0400

    cleaned up code

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,16 +1,15 @@
 // Dependencies
-// =============================================================
 var path = require("path");
 var friends = require('../data/friends.js');
 
 // Routing
-// =============================================================
-module.exports = function(app) {
-  app.get("/api/friends", function(req, res) {
-      return res.json(friends);
-    });
-  
-  app.post("/api/friends", function(req, res) {
+module.exports = function (app) {
+  app.get("/api/friends", function (req, res) {
+    return res.json(friends);
+  });
+
+  // Create New Friend - takes in JSON input
+  app.post("/api/friends", function (req, res) {
     var userData = req.body;
 
     var userScores = userData.scores;
@@ -20,20 +19,23 @@ module.exports = function(app) {
     var totalDiff = 1000;
 
 
-    for(var i = 0; i < friends.length; i++) {
+    for (var i = 0; i < friends.length; i++) {
       var diff = 0;
       for (var j = 0; j < userScores.length; j++) {
         diff += Math.abs(friends[i].scores[j] - userScores[j]);
       }
-     if (diff < totalDiff) {
-       totalDiff = diff;
-       matchName = friends[i].name;
-       matchPic = friends[i].pic;
-     }
+      if (diff < totalDiff) {
+        totalDiff = diff;
+        matchName = friends[i].name;
+        matchPic = friends[i].pic;
+      }
     }
 
+
+    // add new entry to friends array
     friends.push(userData);
 
-    res.json({status: "OK", matchName: matchName, matchPic: matchPic});
+    // returns the compatible friend to be displayed in the modal on survey page
+    res.json({ status: "OK", matchName: matchName, matchPic: matchPic });
   });
 };
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 // Dependencies
-// =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 
+// Tells node that we are creating an "express" server
 var app = express();
+// Sets an initial port. We"ll use this later in our listener
 var PORT = 8080;
 
 // Sets up the Express app to handle data parsing
@@ -17,7 +18,6 @@ require('./app/routing/apiRoutes')(app);
 require('./app/routing/htmlRoutes')(app);
 
 // Starts the server to begin listening
-// =============================================================
 app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
+  console.log("App listening on PORT " + PORT);
 });
